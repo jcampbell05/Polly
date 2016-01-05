384d394a2ff2b01dae2a465376d3beb268fcc4d6
James Campbell <james@supmenow.com>
2016-01-05 20:39:58 +0000

    Fully working Force Touch Polyfill.

--- Pod/Classes/UITraitCollection.h
+++ Pod/Classes/UITraitCollection.h
@@ -6,8 +6,23 @@
 //
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface UITraitCollection : NSObject
+//For iPhone 8.0 SDK
+#ifndef __IPHONE_9_0
+#ifdef __IPHONE_8_0
+
+typedef NS_ENUM(NSInteger, UIForceTouchCapability) {
+    UIForceTouchCapabilityUnknown = 0,
+    UIForceTouchCapabilityUnavailable = 1,
+    UIForceTouchCapabilityAvailable = 2
+};
+
+@interface UITraitCollection (ForceTouchCapabilityPolyfill)
+
+@property(nonatomic, readonly) UIForceTouchCapability forceTouchCapability;
 
 @end
+
+#endif
+#endif
--- Pod/Classes/UITraitCollection.m
+++ Pod/Classes/UITraitCollection.m
@@ -8,6 +8,41 @@
 
 #import "UITraitCollection.h"
 
-@implementation UITraitCollection
+#import <objc/runtime.h>
+
+//For iPhone 8.0 SDK
+#ifndef __IPHONE_9_0
+#ifdef __IPHONE_8_0
+
+@implementation UITraitCollection (ForceTouchCapabilityPolyfill)
+
+@end
+
+#endif
+#endif
+
+UIForceTouchCapability polly_forceTouchCapability (id self, SEL _cmd)
+{
+    return  UIForceTouchCapabilityUnavailable;
+}
+
+@implementation UITraitCollection (ForceTouchCapabilityPolyfill)
+
++ (void)load {
+    
+    if (NSFoundationVersionNumber <= NSFoundationVersionNumber_iOS_8_4)
+    {
+        BOOL success = class_addMethod(self.class,
+                                       @selector(forceTouchCapability),
+                                       (IMP)polly_forceTouchCapability,
+                                       @encode(UIForceTouchCapability));
+        
+        
+        if (!success)
+        {
+            NSLog(@"Couldn't install force touch capability polyfill");
+        }
+    }
+}
 
 @end
